bec313bb65ef4f6339a21f5c2b8ae36e741c02dc
Conde28 <138520940+Conde28@users.noreply.github.com>
2023-08-12 09:33:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <title>Gestión de Usuarios</title>
-    <!-- Puedes agregar aquí tus hojas de estilo o cualquier otro recurso que quieras cargar -->
-</head>
+    </head>
 <body>
 
 <?php require 'views/usuarios.php'; ?>
